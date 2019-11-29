05074aad26278c0f37e8b6353b99ab47061fb558
Frank <frankfer1026@gmail.com>
2019-11-29 14:02:36 -0500

    Initial Commit

---
+++ WordScrambler/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WordScrambler
+//
+//  Created by Frank Fernandez on 11/29/19.
+//  Copyright © 2019 Frank Fernandez. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
